432709143b5f34424f2a56e6bc93b0ef3c763496
N8071 <tanzhikang@corp.netease.com>
2022-05-12 20:54:53 +0800

    push code

--- CppDemo/mySTL/iterator.h
+++ CppDemo/mySTL/iterator.h
@@ -24,8 +24,51 @@ template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T
 struct iterator
 {
     typedef Category    iterator_category;
-    
+    typedef T           value_type;
+    typedef Pointer     pointer;
+    typedef Reference   reference;
+    typedef Distance    difference_type;
 };
+
+template <class T>
+struct has_iterator_cat
+{
+private:
+    struct two { char a; char b;};
+    template <class U> static two test(...);
+    template <class U> static char test(typename U::iterator_category* = 0);
+public:
+    static const bool value = sizeof(test<T>(0)) == sizeof(char);
+};
+
+template <class Iterator, bool>
+struct iterator_traits_impl {};
+
+template <class Iterator>
+struct iterator_traits_impl<Iterator, true>
+{
+    typedef typename Iterator::iterator_category    iterator_category;
+    typedef typename Iterator::value_type           value_type;
+    typedef typename Iterator::pointer              pointer;
+    typedef typename Iterator::reference            reference;
+    typedef typename Iterator::difference_type      difference_type;
+};
+
+template <class Iterator, bool>
+struct iterator_traits_helper {};
+
+template <class Iterator>
+struct iterator_traits_helper<Iterator, true> :
+public iterator_traits_impl<Iterator,
+std::is_convertible<typename Iterator::iterator_category, input_iterator_tag>::value ||
+std::is_convertible<typename Iterator::iterator_category, output_iterator_tag>::value>
+{
+};
+
+template <class Iterator>
+struct iterator_traits : public iterator_traits_helper<Iterator, has_iterator_cat<Iterator>::value> {};
+
+
 }
 
 #endif /* iterator_hpp */
